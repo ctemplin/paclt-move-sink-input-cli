114273d2845327ff50863fc8b75414849fa3699c
Templin, Chris <ctemplin@gmail.com>
2021-05-28 12:09:45 -0400

    Using module to get pactl list as objects Removing regexes

--- pactl-move-sink-input-cli.js
+++ pactl-move-sink-input-cli.js
@@ -1,33 +1,10 @@
 #!/usr/bin/env node
-const { resolve } = require('path');
 const shell = require('shelljs')
 const readline = require('readline');
 const util = require('util')
+const {getInputs, getSinks} = require('pactl-lists-json')
 
-const inputsRg = new RegExp(/Sink Input #(\d+).*Sink: (\d+).*media\.name = "(\w*)"\s*application.name = "(\w*)"/, 'sg')
 const sinksRg = new RegExp(/Sink #(\d*)\s*State: (\w*)\s*[^\n]*\n\sDescription: ([^\n]*)\n/, 'mg')
-
-async function getInputs(){
-    return new Promise((resolve, reject) => {
-        try {
-            const exec = shell.exec('pactl list sink-inputs', {silent: true})
-            if (exec.stderr) {
-                reject(exec.stderr)
-            }
-            resolve(exec.stdout)
-        } catch(error) {
-            reject(error)
-        }
-    });
-}
-
-async function tokenizeInputs(inputsTxt) {
-    return new Promise((resolve, reject) => {
-        const inputArr = []
-        inputsTxt.split('\n\n').filter(Boolean).map(inputTxt=>inputArr.push(Array.from(inputTxt.matchAll(inputsRg))[0]))
-        resolve(inputArr)
-    });
-}
 
 async function getInputChoice(inputArr){
     return new Promise((resolve, reject) => {
@@ -43,7 +20,7 @@ async function getInputChoice(inputArr){
             })
             var inputChoice
             var inputList = 'Multiple inputs found\n'
-            inputArr.map((i, index)=>inputList += index + ': ' + i[4] + '-' + i[3] + '\n')
+            inputArr.map((i, index)=>inputList += util.format('%d: %s - %s\n', index, i['appliction.name'], i['media.name']))*
             rl.setPrompt(inputList + 'Enter # of input to be moved>')
             rl.on('line', line => {
                 if (inputArr.map((i,index) => index).includes(parseInt(line))) {
@@ -62,43 +39,22 @@ async function getInputChoice(inputArr){
     });
 }
 
-async function getSinks(){
-    return new Promise((resolve, reject) => {
-        try {
-            const exec = shell.exec('pactl list sinks', {silent: true})
-            if (exec.stderr) {
-                reject(exec.stderr)
-            }
-            resolve(exec.stdout)
-        } catch(error) {
-            reject(error)
-        }
-    });
-}
-
-async function tokenizeSinks(sinksTxt) {
-    return new Promise((resolve, reject) => {
-        const sinkArr = Array.from(sinksTxt.matchAll(sinksRg))
-        resolve(sinkArr)
-    });
-}
-
 async function getSinkChoice(sinkArr, defInput) {
     return new Promise((resolve, reject) => {
         // Find the sink for the chosen input
-        const curSink = sinkArr.find((i) => i[1] == defInput[2])
+        const curSink = sinkArr.find((i) => i.Sink == defInput.Sink)
         // Remove the active sink from the list of target sinks
         sinkArr.splice(sinkArr.indexOf(curSink), 1)
         // if there's only one non-active sync choice, choose it
         if (sinkArr.length == 1) {
-            const autoMoveMsg = util.format('Moving %s - %s to %s.\nExiting...', defInput[4], defInput[3], sinkArr[0][3])
+            const autoMoveMsg = util.format('Moving %s - %s to %s.\nExiting...', defInput['application.name'], defInput['media.name'], sinkArr[0].Description)
             displayMsgAndPause(autoMoveMsg)
             resolve(sinkArr[0])
         } else {
             // Output info about active input/sink.
-            var promptTxt = util.format('%s - %s - playing on %s\n', defInput[4], defInput[3], curSink[3])
+            var promptTxt = util.format('%s - %s - playing on %s\n', defInput['application.name'], defInput['media.name'], curSink.Description)
             // List/number the non-active sinks
-            sinkArr.forEach((i, index) => { promptTxt += util.format('%s - %s\n', index+1, i[3]) })
+            sinkArr.forEach((i, index) => { promptTxt += util.format('%s - %s\n', index+1, i.Description) })
             promptTxt += 'Enter # of target to move to >'
             // Prompt for new sink number
             var rl = readline.createInterface({
@@ -133,15 +89,13 @@ function displayMsgAndPause(msg){
 
 function main() {
     getInputs()
-    .then(tokenizeInputs)
     .then(getInputChoice)
     .then(function(inputChoice) {
         sinkChoice = getSinks()
-        .then(tokenizeSinks)
         .then(function (sinkArr) {
             getSinkChoice(sinkArr, inputChoice).
             then(function(sinkChoice){
-                shell.exec(util.format('pactl move-sink-input %d %d', inputChoice[1], sinkChoice[1] ))
+                shell.exec(util.format('pactl move-sink-input %d %d', inputChoice['Sink Input'], sinkChoice.Sink ))
             })
             .catch(err=>{displayMsgAndPause(err)})
         })
